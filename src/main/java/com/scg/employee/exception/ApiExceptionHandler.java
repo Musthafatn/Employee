bac79e5b5524cc63aa704ec7b0bfc5173d627b65
musthafa <musthafa769769@gmail.com>
2021-05-26 15:03:16 +0530

    path added in exception

--- src/main/java/com/scg/employee/exception/ApiExceptionHandler.java
+++ src/main/java/com/scg/employee/exception/ApiExceptionHandler.java
@@ -5,23 +5,26 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
 	@ExceptionHandler(ApiException.class)
-	public ResponseMessage handleApiException(final ApiException e) {
+	public ResponseMessage handleApiException(final ApiException e, final WebRequest r) {
 
 		final HttpStatus status = HttpStatus.NOT_FOUND;
-		return new ResponseMessage(LocalDateTime.now(), status.value(), status.name(), e.getMessage(), "/employee/");
+		return new ResponseMessage(LocalDateTime.now(), status.value(), status.name(), e.getMessage(),
+				r.getDescription(false));
 
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseMessage handleException(final Exception e) {
+	public ResponseMessage handleException(final Exception e, final WebRequest r) {
 
 		final HttpStatus status = HttpStatus.BAD_REQUEST;
-		return new ResponseMessage(LocalDateTime.now(), status.value(), status.name(), e.getMessage(), "/employee/");
+		return new ResponseMessage(LocalDateTime.now(), status.value(), status.name(), e.getMessage(),
+				r.getDescription(false));
 
 	}
 
